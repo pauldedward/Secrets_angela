87521343e64fa5ed35e0f7a851a0a49c2ee657b2
unknown <edward2000ed@gmail.com>
2021-04-07 11:51:17 +0530

    port change

--- app.js
+++ app.js
@@ -181,6 +181,6 @@ app.post("/login", function(req, res) {
 
 
 
-app.listen("3000", function() {
+app.listen(process.env.PORT||"3000", function() {
         console.log("up and running in 3000");
 })
